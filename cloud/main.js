81a40fca6e03883f5a931284e5734ec0f36deb79
Yiğit YOL <ayigitd@gmail.com>
2016-10-30 23:12:56 +0200

    register

--- cloud/main.js
+++ cloud/main.js
@@ -27,21 +27,4 @@ Parse.Cloud.define('register', function (req, res) {
 	else{
 		res.success(false);
 	}
-	/*
-	var query = new Parse.Query("Trip");
-	query.include('user');
-	query.equalTo("user", user);
-	query.find({
-		success: function (results) {
-			var sum = 0;
-			for (var i = 0; i < results.length; ++i) {
-				sum += results[i].get("end") - results[i].get("start");
-			}
-			response.success(sum);
-		},
-		error: function () {
-			response.error("trip lookup failed");
-		}
-	});
-	*/
 });
