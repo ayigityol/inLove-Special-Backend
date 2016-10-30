daa961aedbda7edbe4f9ec9237c73c6858e738ab
Yiğit YOL <ayigitd@gmail.com>
2016-10-30 21:59:30 +0200

    v 1.01

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,35 @@
 
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+Parse.Cloud.define('hello', function (req, res) {
+	res.success('Hi');
+});
+
+Parse.Cloud.define('register', function (req, res) {
+
+	try {
+		var user = new Parse.User();
+		user.email = req.params.email;
+		user.password = req.params.password; //todo improve
+		user.username = req.params.username;
+		user.emailVerified = true;
+		res.send(true);
+	} catch (e) {
+		res.send(false);
+	}
+	/*
+	var query = new Parse.Query("Trip");
+	query.include('user');
+	query.equalTo("user", user);
+	query.find({
+		success: function (results) {
+			var sum = 0;
+			for (var i = 0; i < results.length; ++i) {
+				sum += results[i].get("end") - results[i].get("start");
+			}
+			response.success(sum);
+		},
+		error: function () {
+			response.error("trip lookup failed");
+		}
+	});
+	*/
 });
